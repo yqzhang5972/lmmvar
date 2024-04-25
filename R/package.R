34567c1725b018ca7914abb837ecf818c26fd7ff
koekvall <k.o.ekvall@gmail.com>
2024-04-25 09:27:45 -0400

    Return license to new correct form

--- R/package.R
+++ R/package.R
@@ -1,13 +1,10 @@
 #' lmmvar
 #'
-#' compute score functions for 1d and 2d, give confidence interval for proportion of variation, and a confidence grid matrix for proportion of variation and total variation
+#' Computes the score standardized by expected information test statitics and confidence intervals for a variance component or proportion.
 #'
-#' @docType package
-#' @author yiqiao zhang <yqzhang5972 at outlook.com>
 #' @import Rcpp RcppEigen
 #' @useDynLib lmmvar, .registration = TRUE
 #' @name lmmvar
-#' @aliases <lmmvar>-package
 #' @importFrom Rcpp evalCpp
 
 NULL
