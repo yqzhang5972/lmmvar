42ed8d719f4b9c0a59a3dea1a71049dd8b621f65
koekvall <k.o.ekvall@gmail.com>
2024-10-26 08:39:30 -0400

    New documentation

--- R/lmmvar-package.R
+++ R/lmmvar-package.R
@@ -2,12 +2,10 @@
 #'
 #' compute score functions for 1d and 2d, give confidence interval for proportion of variation, and a confidence grid matrix for proportion of variation and total variation
 #'
-#' @docType package
 #' @author yiqiao zhang <yqzhang5972 at outlook.com>
 #' @import Rcpp RcppEigen
 #' @useDynLib lmmvar, .registration = TRUE
 #' @name lmmvar
-#' @aliases <lmmvar>-package
+#' @aliases lmmvar-package
 #' @importFrom Rcpp evalCpp
-
-NULL
+"_PACKAGE"
