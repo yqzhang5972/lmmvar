89c8103d80aa91f57274e55ecb3b50a762e5647b
yiqiao zhang <yqzhang5972@outlook.com>
2024-10-30 02:29:32 -0400

    change grid method, need to add description

--- R/RLRTCI.R
+++ R/RLRTCI.R
@@ -1,6 +1,3 @@
-library(Rcpp)
-# sourceCpp("RLRTSim.cpp")
-
 # Take lower triangle Cholesky decomposition L of A and compute solve(A, B)
 chol_solve <- function(L, B){
   forwardsolve(L, forwardsolve(L, B), transpose = TRUE)
@@ -10,9 +7,8 @@ chol_solve <- function(L, B){
 #'@import Rcpp
 #'@importFrom stats rchisq
 #'@export RLRTSim
-RLRTSim <- function(X, ZSigmasqrt, lambda0 = NA, nsim = 10000, log.grid.hi = 8, # ZSigmasqrt n*m, tcrossprod(ZSigmasqrt)=K
-                    seed = NA,
-                    log.grid.lo = -10, gridlength = 200) {
+RLRTSim <- function(X, ZSigmasqrt, tau0 = NA, nsim = 10000, # ZSigmasqrt n*m, tcrossprod(ZSigmasqrt)=K
+                    seed = NA, grid = 200) {
   n <- nrow(X)
   p <- ncol(X)
   m <- ncol(ZSigmasqrt)
@@ -21,61 +17,19 @@ RLRTSim <- function(X, ZSigmasqrt, lambda0 = NA, nsim = 10000, log.grid.hi = 8, 
   if (!is.na(seed))
     set.seed(seed)
 
-  #generate symmetric grid around lambda0 that is log-equidistant to the right,
-  make.lambdagrid <- function(lambda0, gridlength, log.grid.lo, log.grid.hi) {
+  h2seq <- seq(0, 1- tolerate, length.out = grid)
+  tauGrid <- h2seq / (1-h2seq)
 
-    # return(c(0, exp(seq(log.grid.lo, log.grid.hi,
-    #  length = gridlength - 1))))
-
-    if (lambda0 == 0)
-      return(c(0, exp(seq(log.grid.lo, log.grid.hi,
-                          length = gridlength - 1))))
-    else {
-      leftratio <- min(max((log(lambda0)/((log.grid.hi) - (log.grid.lo))),
-                           0.2), 0.8)
-      leftlength <- max(round(leftratio * gridlength) - 1, 2)
-      leftdistance <- lambda0 - exp(log.grid.lo)
-      #make sure leftlength doesn't split the left side into too small parts:
-      if (leftdistance < (leftlength * 10 * .Machine$double.eps)) {
-        leftlength <- max(round(leftdistance/(10 * .Machine$double.eps)), 2)
-      }
-      #leftdistance approx. 1 ==> make a regular grid, since
-      # (1 +- epsilon)^((1:n)/n) makes a too concentrated grid
-      if (abs(leftdistance - 1) < 0.3) {
-        leftgrid <- seq(exp(log.grid.lo), lambda0,
-                        length = leftlength + 1)[-(leftlength + 1)]
-      }
-      else {
-        leftdiffs <- ifelse(rep(leftdistance > 1, leftlength - 1),
-                            leftdistance^((2:leftlength)/leftlength) -
-                              leftdistance^(1/leftlength),
-                            leftdistance^((leftlength - 1):1) -
-                              leftdistance^(leftlength))
-        leftgrid <- lambda0 - rev(leftdiffs)
-      }
-      rightlength <- gridlength - leftlength
-      rightdistance <- exp(log.grid.hi) - lambda0
-      rightdiffs <- rightdistance^((2:rightlength)/rightlength) -
-        rightdistance^(1/rightlength)
-      rightgrid <- lambda0 + rightdiffs
-      return(c(0, leftgrid, lambda0, rightgrid))
-    }
-  }
-  lambda.grid <- make.lambdagrid(lambda0, gridlength, log.grid.lo = log.grid.lo,
-                                 log.grid.hi = log.grid.hi)
-
-  res <- RLRsimCpp(p = as.integer(p), m = as.integer(m),
-                   n = as.integer(n), nsim = as.integer(nsim),
-                   gridlength = as.integer(gridlength),
-                   mu = as.double(mu),
-                   lambdaGrid = as.double(lambda.grid),
-                   lambda0 = as.double(lambda0))
-
-  return(res)
+  return(
+    RLRsimCpp(p = p, m = m, n = n,
+              nsim = nsim, mu = mu,
+              tauGrid = tauGrid,
+              tau0 = tau0)
+    )
 }
 
 # negative log-likelihood function for new parameterization
-neg_loglik2_repar <- function(par, X, y, eigens) { # par = lambda, sigma^2_e
+neg_loglik2_repar <- function(par, X, y, eigens) { # par = tau, sigma^2_e
   n <- nrow(X)
   Sigma <- (par[1] * eigens + rep(1, n)) * par[2]
   Sigma_inv <- 1 / Sigma
@@ -87,7 +41,7 @@ neg_loglik2_repar <- function(par, X, y, eigens) { # par = lambda, sigma^2_e
   return(l)
 }
 
-neg_loglik1_repar <- function(par, X, y, eigens) { # par = lambda0
+neg_loglik1_repar <- function(par, X, y, eigens) { # par = tau0
   n <- nrow(X)
   p <- ncol(X)
   Sigma <- (par * eigens + rep(1, n))     # \Sigma / sigma_e^2
@@ -104,15 +58,15 @@ neg_loglik1_repar <- function(par, X, y, eigens) { # par = lambda0
   return(l)
 }
 
-# function of wald and LRT test statistics for multi-core computing, k: iteration
-rlrt <- function(Xnew, ynew, eigens, lambda0) {
+# function to calculate observed rlrt
+rlrt <- function(Xnew, ynew, eigens, tau0) {
   # Ha
   opt <- stats::optim(par=c(1,1), neg_loglik2_repar, X=Xnew, y=ynew,
                       eigens=eigens, method = "L-BFGS-B",
                       lower = c(1e-4, 1e-4),
                       upper = c(Inf, Inf)) # , control = list(trace = 10)
   # H0
-  neglog <- neg_loglik1_repar(lambda0, X=Xnew, y=ynew, eigens=eigens)
+  neglog <- neg_loglik1_repar(tau0, X=Xnew, y=ynew, eigens=eigens)
   rlrt <- 2 * (neglog - opt$value)
   return(rlrt[1,1])
 }
@@ -125,7 +79,7 @@ RLRTCI <- function(SimDists, ciseq, Xnew, ynew, eigens, alpha=0.05) {
   ifinCI <- rep(0, length(ciseq))
   for (ic in 1:length(ciseq)) {
     rlrt.obs <- rlrt(Xnew, ynew, eigens, ciseq[ic])
-    ifinCI[ic] <- mean(rlrt.obs < SimDists[[ic]]$simDist) >= alpha
+    ifinCI[ic] <- mean(rlrt.obs < SimDists[[ic]]) >= alpha
   }
   ci <- c(min(ciseq[ifinCI != 0]), max(ciseq[ifinCI != 0]))
   return(ci)
--- src/RLRTSim.cpp
+++ src/RLRTSim.cpp
@@ -4,62 +4,48 @@
 using namespace Rcpp  ;
 
 // [[Rcpp::export]]
-List RLRsimCpp (
-    int p,
-    int m,
-    int n,
-    int nsim,
-    int gridlength,
-    Rcpp::NumericVector mu,
-    Rcpp::NumericVector lambdaGrid,
-    double lambda0) {
+Rcpp::NumericVector RLRsimCpp (
+    int p, int m, int n, int nsim,
+    Rcpp::NumericVector mu, Rcpp::NumericVector tauGrid, double tau0) {
 
-  int is, ig, im;
+  int grid = tauGrid.length();
+  Rcpp::NumericMatrix denom(grid, m), Nmat(grid, m), Dmat(grid, m);
+  Rcpp::NumericVector negterm2(grid), chisq(m), simDist(nsim);
   double N, D, F;
-  Rcpp::NumericMatrix deno(gridlength, m) ;
-  Rcpp::NumericMatrix fN(gridlength, m) ;
-  Rcpp::NumericMatrix fD(gridlength, m) ;
-
-  Rcpp::NumericVector negterm2(gridlength) ;
-  Rcpp::NumericVector Chiw(m) ;
-  Rcpp::IntegerVector maxIndex(nsim) ;
-  Rcpp::NumericVector simDist(nsim) ;
 
   // without simulation
-  for(ig = 0; ig < gridlength; ++ig) {
+  for(int ig = 0; ig < grid; ig++) {
     negterm2[ig] = 0 ;
-    for(im=0 ; im < m ; ++im){
-      deno(ig, im) = lambdaGrid[ig] * mu[im] + 1.0 ;
+    for(int im=0 ; im < m ; im++){
+      denom(ig, im) = tauGrid[ig] * mu[im] + 1.0;
 
-      fN(ig, im) = ((lambdaGrid[ig] - lambda0) * mu[im]) / deno(ig, im) ;
-      fD(ig, im) = (1 + lambda0 * mu[im]) / deno(ig, im) ;
-      negterm2[ig] += log(fD(ig, im)) ;
+      Nmat(ig, im) = ((tauGrid[ig] - tau0) * mu[im]) / denom(ig, im);
+      Dmat(ig, im) = (1 + tau0 * mu[im]) / denom(ig, im);
+      negterm2[ig] += log(Dmat(ig, im));
     }
   }
   //print(negterm2) ;
 
   //begin simulation
-  for(is = 0; is < nsim; ++is) {
-    Chiw = rchisq(m, 1) ;
-    simDist[is] = R_NegInf ;
-    for(ig = 0; ig < gridlength; ++ig) {
+  for(int is = 0; is < nsim; is++) {
+    chisq = rchisq(m, 1);
+    simDist[is] = R_NegInf;
+
+    for(int ig = 0; ig < grid; ig++) {
       N = D = 0 ;
 
-      for(im=0 ; im < m ; ++im){
-        N += fN(ig, im) * Chiw[im] ;
-        D += fD(ig, im) * Chiw[im] ;
+      for(int im=0 ; im < m ; im++){
+        N += Nmat(ig, im) * chisq[im];
+        D += Dmat(ig, im) * chisq[im];
       }
-      F = (n - p) * log1p(N/D) + negterm2[ig] ;
-      //Rcout<<F<<" "<<simDist[is]<<' '<<(F>= simDist[is])<<' ' ;
+      F = (n - p) * log1p(N/D) + negterm2[ig];
+      //Rcout<<F<<" "<<simDist[is]<<' '<<(F>= simDist[is])<<' ';
 
       if(F >= simDist[is]){
-        //Rcout<<"YES"<< simDist[is]<<' ' ;
-        simDist[is] = F ;
-        maxIndex[is] = ig + 1 ;
-      } //else break ;
+        simDist[is] = F;
+      };
     }
   }
-  return List::create(Named("simDist")=simDist,
-                      Named("maxIndex")=maxIndex) ;
+  return simDist;
 
 }
